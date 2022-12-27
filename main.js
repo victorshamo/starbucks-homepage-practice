3a00e170cbc28534b6f6b4f9f6cbf0f5465c4491
victorshamo <37343169+victorshamo@users.noreply.github.com>
2022-12-27 18:35:53 -0300

    fix: Mobile Menu

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
 
 function navToggle() {
     //will add a class of 'open' to it
     btn.classList.toggle('open')
+    nav.classList.toggle('hidden')
+    document.body.classList.toggle('no-scroll')
 }
 
 
